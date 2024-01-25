1018d4aa44af48ee74bc6622f9421df3b37d1690
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2024-01-25 22:29:47 +0200

    finish

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-// ejs template example
+const express = require('express'),
+	morgan = require('morgan'),
+	path = require('path')
 
-const express = require('express');
-const app = express();
+const app = express(),
+	port = 3001
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs-mate'))
 
-const users = [
-  {
-    name: 'Tedy',
-    age: 20,
-    species: 'student',
-  },
-  {
-    name: 'Adam',
-    age: 32,
-    species: 'worker',
-  },
-];
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-  res.render('index', { users });
-});
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.use('/', require('./routes'))
+app.use('/catalog', require('./routes/catalog'))
+app.use('/user', require('./routes/user'))
+
+app.listen(port, () => {
+	console.log('server started on http://localhost:' + port)
+})
